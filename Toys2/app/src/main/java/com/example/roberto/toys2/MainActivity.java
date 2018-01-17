cbeebef42dae3baba5322b1fbcb23d702d08bb79
Roberto <robertopc@github.com>
2018-01-17 20:20:24 -0300

    update
    update 17/01

--- Toys2/app/src/main/java/com/example/roberto/toys2/MainActivity.java
+++ Toys2/app/src/main/java/com/example/roberto/toys2/MainActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Random;
 import java.util.UUID;
 
@@ -25,12 +27,13 @@ import static java.lang.String.valueOf;
 public class MainActivity extends AppCompatActivity {
     private static final int SOLICITA_ATIVACAO = 1;
     private static final int SOLICITA_CONEXAO = 2;
+    ConnectedThread connectedThread;
     BluetoothAdapter toyBluetoothAdapter = null;
     BluetoothDevice toyDevice = null;
     BluetoothSocket toySocket = null;
     Spinner animais;
     ListView silabas;
-    Button btnaleatorio, btnConexao;
+    Button btnaleatorio, btnConexao, btnEscolher;
     TextView display_data;
     Context context = this;
     boolean conexao = false;
@@ -73,6 +76,17 @@ public class MainActivity extends AppCompatActivity {
                 Intent openlist = new Intent(MainActivity.this, DevicesList.class);
                 startActivityForResult(openlist, SOLICITA_CONEXAO);
             }
+            }
+        });
+
+        btnEscolher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(conexao) {
+                    connectedThread.enviar("");
+                } else {
+                    Toast.makeText(getApplicationContext(), "O bluetooth não está conectado", Toast.LENGTH_LONG).show();
+                }
             }
         });
         animais = (Spinner) findViewById(R.id.spinneranimal);
@@ -124,6 +138,8 @@ public class MainActivity extends AppCompatActivity {
                         toySocket = toyDevice.createRfcommSocketToServiceRecord(TOY_UUID);
                         toySocket.connect();
                         conexao = true;
+                        connectedThread = new ConnectedThread(toySocket);
+                        connectedThread.start();
                         btnConexao.setText("Desconectar");
                         Toast.makeText(getApplicationContext(), "Conectado com: " + MAC, Toast.LENGTH_LONG).show();
 
@@ -136,5 +152,49 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Falha na conexão", Toast.LENGTH_LONG).show();
                 }
         }
+    }private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+           /* while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }*/
+        }
+
+        public void enviar(String dadosEnviar) {
+            byte[] msgBuffer = dadosEnviar.getBytes();
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) { }
+        }
+
     }
 }
