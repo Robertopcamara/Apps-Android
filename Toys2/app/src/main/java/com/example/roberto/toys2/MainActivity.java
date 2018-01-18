67b782279e488028396ecc7b5251e80f41e17e58
Roberto <robertopc@github.com>
2018-01-17 21:28:24 -0300

    up

--- Toys2/app/src/main/java/com/example/roberto/toys2/MainActivity.java
+++ Toys2/app/src/main/java/com/example/roberto/toys2/MainActivity.java
@@ -6,8 +6,11 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.Buffer;
 import java.util.Random;
 import java.util.UUID;
 
@@ -27,7 +31,10 @@ import static java.lang.String.valueOf;
 public class MainActivity extends AppCompatActivity {
     private static final int SOLICITA_ATIVACAO = 1;
     private static final int SOLICITA_CONEXAO = 2;
+    private static final int MESSAGE_READ = 3;
     ConnectedThread connectedThread;
+    Handler mHandler;
+    StringBuilder dadosBluetooth = new StringBuilder();
     BluetoothAdapter toyBluetoothAdapter = null;
     BluetoothDevice toyDevice = null;
     BluetoothSocket toySocket = null;
@@ -89,6 +96,31 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if(msg.what == MESSAGE_READ) {
+                    String recebidos = (String) msg.obj;
+                    dadosBluetooth.append(recebidos);
+
+                    int fimInfo = dadosBluetooth.indexOf("}");
+
+                    if(fimInfo > 0){
+                        String dadosCompletos = dadosBluetooth.substring(0, fimInfo);
+
+                        int tamanhoInfo = dadosCompletos.length();
+
+                        if(dadosBluetooth.charAt(0) == '{') {
+                            String dadosFinais = dadosBluetooth.substring(1, tamanhoInfo);
+                            Log.d("Recebidos", dadosFinais);
+                        }
+                        dadosBluetooth.delete(0, dadosBluetooth.length());
+
+                    }
+                }
+
+            }
+        };
         animais = (Spinner) findViewById(R.id.spinneranimal);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.animais_array, android.R.layout.simple_spinner_item);
         animais.setAdapter(adapter);
@@ -176,17 +208,19 @@ public class MainActivity extends AppCompatActivity {
             int bytes; // bytes returned from read()
 
             // Keep listening to the InputStream until an exception occurs
-           /* while (true) {
+            while (true) {
                 try {
                     // Read from the InputStream
                     bytes = mmInStream.read(buffer);
+
+                    String dadosBt = new String(buffer, 0, bytes);
+
                     // Send the obtained bytes to the UI activity
-                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
-                            .sendToTarget();
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, dadosBt).sendToTarget();
                 } catch (IOException e) {
                     break;
                 }
-            }*/
+            }
         }
 
         public void enviar(String dadosEnviar) {
